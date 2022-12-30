da04546f93a5783a3b8ea9da7ab36dd197f2d41a
Liam <109904413+Liamlol1919@users.noreply.github.com>
2022-12-30 13:06:24 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+function fuc() {
+    let pop = window.pageYOffset;
+    document.getElementById('bigtitle').style.backdropFilter = `blur(${pop/150}px)`;
+    document.getElementById('bigtitle').style.filter = `blur(${pop/200}px)`;
+    document.getElementById('bigtitle').style.fontSize = `${50-pop/100}px`;
+    let inn = window.innerHeight
+    document.getElementById('s2').style.borderTopLeftRadius = `${16-pop/(inn/30)}px`;
+    document.getElementById('s2').style.borderTopRightRadius = `${16-pop/(inn/30)}px`;
+    let perc = 5+pop/inn*20;
+    document.getElementById('s2').style.backgroundColor = `hsl(231,97%,${Math.min(15,perc)}%)`
+    
+    document.querySelector('h2').style.filter = `blur(${0.5-pop/inn}px)`;
+    document.querySelector('p').style.filter = `blur(${0.5-pop/inn}px)`;
+    document.querySelector('h3').style.filter = `blur(${0.5-pop/inn}px)`;
+    document.querySelector('.h3').style.filter = `blur(${0.5-pop/inn}px)`;
+    document.querySelector('#foc').style.filter = `blur(${0.5-pop/inn}px)`;
+    document.querySelector('#t2').style.filter = `blur(${0.5-pop/inn}px)`;
+    document.querySelector('#t3').style.filter = `blur(${0.5-pop/inn}px)`;
+}
